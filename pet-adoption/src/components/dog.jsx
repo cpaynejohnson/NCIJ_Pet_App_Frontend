e95be635e56d4c415a03235e7c86d2904db22456
cpaynejohnson <cpaynejohnson@gmail.com>
2022-02-09 13:12:16 -0600

    added card to dog.jsx and corrected color in app.css

--- pet-adoption/src/components/dog.jsx
+++ pet-adoption/src/components/dog.jsx
@@ -1,19 +1,30 @@
 import React, {  } from 'react';
-import {Link} from 'react-router-dom';
+
+import {Card, Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Dog(props) {
     
     
-    //render the sauce details
+    //render the dog details
     return (
         <div className='Dog'>
-        <h2 id="dog-name">{props.dog.name}</h2>
-        <img className="dog-img" src={props.dog.image} alt={props.dog.name} />
-        <br/>
-        <Link to={`/dogs/${props.dog.id}`}>Meet {props.dog.name}</Link>
+
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={props.dog.image} />
+            <Card.Body>
+                <Card.Title>{props.dog.name}</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
+                <Button href={`/dogs/${props.dog.id}`} variant="primary">Meet {props.dog.name}</Button>
+            </Card.Body>
+           </Card>    
         
         </div>
     );
 }
 
 export default Dog;
+
