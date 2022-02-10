558289cfbdfa6ee0e2501e86456097538d2a08a2
cpaynejohnson <cpaynejohnson@gmail.com>
2022-02-10 17:03:53 -0600

    added new dog, modified dog.jsx

--- pet-adoption/src/components/dog.jsx
+++ pet-adoption/src/components/dog.jsx
@@ -15,8 +15,7 @@ function Dog(props) {
             <Card.Body>
                 <Card.Title>{props.dog.name}</Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                {props.dog.tidbit}
                 </Card.Text>
                 <Button href={`/dogs/${props.dog.id}`} variant="primary">Meet {props.dog.name}</Button>
             </Card.Body>
