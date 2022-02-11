a95cd08e67fe732aa6a9fcf9ef2402c9167dcd3d
Diba <dibaja@ad.vzwcorp.com>
2022-02-10 21:47:18 -0600

    App.css modified

--- pet-adoption/src/components/Footer.jsx
+++ pet-adoption/src/components/Footer.jsx
@@ -5,9 +5,9 @@ import { Container } from 'react-bootstrap';
 function Footer() {
     return (
         <div>
-            <footer class="Head py-2 mt-5 mb-0">
+            <footer className="Head py-2 mt-5 mb-0">
             <Container className='px-5 pt-2'>
-                <p class="text-center text-white">
+                <p className="text-center text-white">
                 Copyright &copy; NCIJ Pet Adoptions 2022
                 </p>
             </Container>
--- pet-adoption/src/components/Header.jsx
+++ pet-adoption/src/components/Header.jsx
@@ -7,22 +7,20 @@ function Header() {
         <div className="Head">
           <header>
             <Navbar variant="dark" expand="lg">
-            <Container>
+            <Container className='Navbar-container'>
+              <div>
               <Navbar.Brand href="#home">NCIJ Pet Adoptions</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              </div>
+              <div>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                   <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                  </NavDropdown>
+                  <Nav.Link href="#link">About Us</Nav.Link>
+                  <Nav.Link href="#link">Contact Us</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
+              </div>
             </Container>
           </Navbar>
           </header>
